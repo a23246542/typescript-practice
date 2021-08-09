71f3bb174b4dee7a57404b48c525e358b9d9abfc
a23246542 <a23246542@gmail.com>
2021-08-10 01:01:52 +0800

    測試2

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import './App.css';
 import { ProjectList } from './screens/project-list';
+import Login from './screens/login';
 
 function App() {
-  let a = 'xxx';
-      console.log(a);
-
   return (
-    <div className="App" ><header className="App-header">
-      <ProjectList />
-    </header>
+    <div className='App'>
+      <header className='App-header'>
+        {/* <ProjectList /> */}
+        <Login />
+      </header>
     </div>
   );
 }
