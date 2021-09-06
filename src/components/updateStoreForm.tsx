09488f40a56864d93dea45f875b93297c05c61b6
a23246542 <a23246542@gmail.com>
2021-09-06 23:55:45 +0800

    測試加featureB

--- src/components/updateStoreForm.tsx
+++ src/components/updateStoreForm.tsx
@@ -29,55 +29,55 @@
 // const PaymentTypeArr: PaymentType[] = Object.values(PaymentType).filter(type=>type!=="");
 // const PaymentTypeArr: PaymentType[] = Object.values(PaymentType);
 
-const paymentTypeArr = Object.entries(copyData.paymentSetting).reduce(
-  (acc, [key, booleanValue], currentIndex) => {
-    if (booleanValue) {
-      switch (key) {
-        case AllowType.CASH:
-          return acc.push(PaymentType.CASH);
-        case AllowType.CREDIT:
-          return acc.push(PaymentType.CREDIT);
-        case AllowType.LINE:
-          return acc.push(PaymentType.LINEPAY);
-        default:
-          break;
-      }
-    }
-  },
-  [] as PaymentType[],
-);
+// const paymentTypeArr = Object.entries(copyData.paymentSetting).reduce(
+//   (acc, [key, booleanValue], currentIndex) => {
+//     if (booleanValue) {
+//       switch (key) {
+//         case AllowType.CASH:
+//           return acc.push(PaymentType.CASH);
+//         case AllowType.CREDIT:
+//           return acc.push(PaymentType.CREDIT);
+//         case AllowType.LINE:
+//           return acc.push(PaymentType.LINEPAY);
+//         default:
+//           break;
+//       }
+//     }
+//   },
+//   [] as PaymentType[],
+// );
 
-if (!copyData.hasOwnProperty('paymentSetting')) {
-  copyData.paymentSetting = {
-    allowCashPay: false,
-    allowCreditPay: false,
-    allowLinePay: false,
-  };
-}
+// if (!copyData.hasOwnProperty('paymentSetting')) {
+//   copyData.paymentSetting = {
+//     allowCashPay: false,
+//     allowCreditPay: false,
+//     allowLinePay: false,
+//   };
+// }
 
-const paymentTypes = Object.entries(copyData.paymentSetting).reduce(
-  (acc, [key, booleanValue]: [string, boolean]): PaymentType[] | [] => {
-    if (!booleanValue) {
-      return acc;
-    }
+// const paymentTypes = Object.entries(copyData.paymentSetting).reduce(
+//   (acc, [key, booleanValue]: [string, boolean]): PaymentType[] | [] => {
+//     if (!booleanValue) {
+//       return acc;
+//     }
 
-    switch (key) {
-      case AllowType.CASH:
-        acc.push(PaymentType.CASH);
-        break;
-      case AllowType.CREDIT:
-        acc.push(PaymentType.CREDIT);
-        break;
-      case AllowType.LINE:
-        acc.push(PaymentType.LINEPAY);
-        break;
-      default:
-        break;
-    }
-    return acc;
-  },
-  [] as PaymentType[],
-);
+//     switch (key) {
+//       case AllowType.CASH:
+//         acc.push(PaymentType.CASH);
+//         break;
+//       case AllowType.CREDIT:
+//         acc.push(PaymentType.CREDIT);
+//         break;
+//       case AllowType.LINE:
+//         acc.push(PaymentType.LINEPAY);
+//         break;
+//       default:
+//         break;
+//     }
+//     return acc;
+//   },
+//   [] as PaymentType[],
+// );
 
-copyData.paymentTypes = paymentTypes;
-const { paymentSetting, ...updateStoreReqData } = copyData;
+// copyData.paymentTypes = paymentTypes;
+// const { paymentSetting, ...updateStoreReqData } = copyData;
